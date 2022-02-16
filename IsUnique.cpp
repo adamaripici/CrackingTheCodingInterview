a016c8db07cfa59a63e6f0a7053c0085981176ad
Ada Pici <55006272+adamaripici@users.noreply.github.com>
2022-02-16 12:13:01 -0600

    Add files via upload

---
+++ IsUnique.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+bool uniqueCharacters(string str)
+{
+  sort(str.begin(),str.end());
+  for (int i = 0; i < str.length(); i ++) {
+    if (str[i] == str[i+1]) {
+      return false;
+    } 
+  }
+  return true;
+}
+int main()
+{
+    string str = "This IS a string";
+ 
+    if (uniqueCharacters(str)) {
+        cout << "The String " << str
+             << " has all unique characters\n";
+    }
+    else {
+        cout << "The String " << str
+             << " has duplicate characters\n";
+    }
+    return 0;
+}
