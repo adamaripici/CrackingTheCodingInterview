d48ab260ecd70d7be6a4b90937542c3ad00ed5a5
Ada Pici <55006272+adamaripici@users.noreply.github.com>
2022-02-16 12:13:56 -0600

    Update IsUnique.cpp

--- IsUnique.cpp
+++ IsUnique.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-
+//1.1) Implement an algorithms to determine if a string has all unique characters.
 bool uniqueCharacters(string str)
 {
   sort(str.begin(),str.end());
